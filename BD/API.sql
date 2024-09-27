81acbb58f500c7f0df848a1256d8c6c0a9138e41
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-09-27 08:31:43 -0300

    :sparkles: feat(sql): Adicionando novas tabelas ao código

--- BD/API.sql
+++ BD/API.sql
@@ -13,3 +13,17 @@ nome varchar(100)
 idFuncao int,
 foreign key (idFuncao) references funcao(ID_funcao)
 );
+
+create table patologias (
+codigo_cid int primary key,
+grau int,
+nome varchar(100)
+);
+
+create table paciente (
+nome varchar(100),
+sexo char (1),
+cpf_paciente bigint primary key,
+codigo_cid int,
+foreign key(codigo_cid) references patologias(codigo_cid)
+);
