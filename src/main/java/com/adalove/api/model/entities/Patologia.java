f28c03dab0b44e184bc82d1f5b8d06bf8175b727
Angelina Borroni <166933492+borroniff@users.noreply.github.com>
2024-10-17 11:42:00 -0300

    A2S-12 ✨ Adicionando Patologia

---
+++ src/main/java/com/adalove/api/model/entities/Patologia.java
@@ -0,0 +1,46 @@
+package org.example.model.entities;
+
+public class Patologia {
+    private String cid;
+    private int grau;
+    private String nome;
+
+    public Patologia(String cid, int grau, String nome) {
+        this.cid = cid;
+        this.grau = grau;
+        this.nome = nome;
+    }
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {
+        this.cid = cid;
+    }
+
+    public int getGrau() {
+        return grau;
+    }
+
+    public void setGrau(int grau) {
+        this.grau = grau;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Patologia{" +
+                "cid=" + cid +
+                ", grau=" + grau +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
+}
