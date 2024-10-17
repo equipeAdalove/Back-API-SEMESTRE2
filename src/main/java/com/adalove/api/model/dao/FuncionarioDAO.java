d407982effefb449e7303dcc55e533b68812a0d1
Angelina Borroni <166933492+borroniff@users.noreply.github.com>
2024-10-17 11:31:11 -0300

    A2S-12 👌 Atualizando FuncionarioDAO

--- src/main/java/com/adalove/api/model/dao/FuncionarioDAO.java
+++ src/main/java/com/adalove/api/model/dao/FuncionarioDAO.java
@@ -1,4 +1,67 @@
-package com.adalove.api.model.dao;
+package org.example.model.DAO;
+
+import org.example.model.entities.Funcionario;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FuncionarioDAO {
+
+    public void create(Funcionario funcionario) {
+        String query = "INSERT INTO funcionario (nome, cargo, crm) VALUES (?, ?, ?)";
+        try (Connection conn = ConnectionFactory.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, funcionario.getNome());
+            ps.setString(2, funcionario.getCargo());
+            ps.setString(3, funcionario.getCrm());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Funcionario> read() {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        String sql = "SELECT nome, cargo, crm FROM funcionario";
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Funcionario funcionario = new Funcionario(
+                        rs.getString("nome"),
+                        rs.getString("cargo"),
+                        rs.getString("crm")
+                );
+                funcionarios.add(funcionario);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return funcionarios;
+    }
+
+    public void update(Funcionario funcionario, int id) {
+        String query = "update funcionario set nome = ?, cargo = ?, crm = ? WHERE id_funcionario = ?";
+        try (Connection conn = ConnectionFactory.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, funcionario.getNome());
+            ps.setString(2, funcionario.getCargo());
+            ps.setString(3, funcionario.getCrm());
+            ps.setInt(4, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void delete(int id) {
+        String query = "DELETE FROM funcionario WHERE id_funcionario = ?";
+        try (Connection conn = ConnectionFactory.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setInt(1, id);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
