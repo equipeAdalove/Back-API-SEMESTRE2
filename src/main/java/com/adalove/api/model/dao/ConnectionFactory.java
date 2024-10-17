9f99498a838ac0140c7e1fda474674d29933f4fa
Angelina Borroni <166933492+borroniff@users.noreply.github.com>
2024-10-17 11:26:52 -0300

    A2S-12 :ok_hand: Atualizando ConnectionFactory

--- src/main/java/com/adalove/api/model/dao/ConnectionFactory.java
+++ src/main/java/com/adalove/api/model/dao/ConnectionFactory.java
@@ -1,14 +1,15 @@
-package com.adalove.api.model.dao;
+package org.example.model.DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public static Connection getConnection() {
-        try{
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/adalove","root","fatec");
-        }catch (SQLException e) {
+    public static Connection getConnection(){
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/api", "root", "cleo2018");
+        }
+        catch (SQLException e){
             throw new RuntimeException(e);
         }
     }
