80b686c7daf419302deb02a346ca52c8e7795f53
Angelina Borroni <166933492+borroniff@users.noreply.github.com>
2024-10-17 11:39:36 -0300

    A2S-12 👌 Atualizando Funcionario

--- src/main/java/com/adalove/api/model/entities/Funcionario.java
+++ src/main/java/com/adalove/api/model/entities/Funcionario.java
@@ -1,4 +1,38 @@
-package com.adalove.api.model.entities;
+package org.example.model.entities;
 
 public class Funcionario {
+
+    private String nome;
+    private String cargo;
+    private String crm;
+
+    public Funcionario(String nome, String cargo, String crm) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.crm = crm;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
 }
