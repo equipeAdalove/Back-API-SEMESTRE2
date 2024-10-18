a1f9a79123b7e6671e07e32b61ecaaf84e3bc9fd
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-10-18 07:32:46 -0300

    A2S-13 :bug: fix: Resolvendo problemas no BD

--- BD/API.sql
+++ BD/API.sql
@@ -1,47 +1,8 @@
-create database api;
-use api;
+CREATE DATABASE api;
+USE api;
 
-create table funcao (
-ID_funcao int primary key,
-cargo varchar(100)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
 );
-
-create table funcionario (
-ID_funcionario int primary key,
-sexo char(1),
-nome varchar(100)
-idFuncao int,
-foreign key (idFuncao) references funcao(ID_funcao)
-);
-
-create table patologias (
-codigo_cid int primary key,
-grau int,
-nome varchar(100)
-);
-
-create table paciente (
-nome varchar(100),
-sexo char (1),
-cpf_paciente bigint primary key,
-codigo_cid int,
-foreign key(codigo_cid) references patologias(codigo_cid)
-);
-
-create table atende (
-ID_funcionario int,
-cpf_paciente int,
-primary key (ID_funcionario, cpf_paciente),
-foreign key(ID_funcionario) references funcionario(ID_funcionario),
-foreign key(cpf_paciente) references paciente(cpf_paciente)
-); 
-
-create table possui (
-cpf_paciente bigint,
-codigo_cid int,
-primary key (cpf_paciente, codigo_cid),
-foreign key(cpf_paciente) references paciente(cpf_paciente),
-foreign key(codigo_cid) references patologias(codigo_cid)
-);
-
-show tables;
