ea5791a20c8d68eec226528c6d921e1b069db329
Vitor Ribeiro <161026545+ribeirovitor04@users.noreply.github.com>
2024-10-18 07:33:20 -0300

    A2S-13 :bug: fix: Resolvendo problemas no BD

--- BD/API.sql
+++ BD/API.sql
@@ -6,3 +6,12 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
 );
+
+INSERT INTO users (username, password) VALUES ('root', '$2a$12$oz7UDRWd/7x1g/biUQSoN.jrqG4oGRam2QF/nrC9L/g6I/eg2.a5S');
+
+CREATE TABLE funcionario (
+    id_funcionario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    cargo VARCHAR(50), 
+    crm VARCHAR(15), Para médicos (não obrigatório)
+);
